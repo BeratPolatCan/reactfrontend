b967fa5af9f155103051fd16a38d6fc95407b285
VictimofSloth <beratpolatcanresmi@gmail.com>
2026-07-03 16:45:35 +0300

    son hali

---
+++ project_2/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const enviroment={
+    production: true,
+    apiUrl: "https://springboot-production-7df8.up.railway.app/api/v1",
+};
---
+++ project_2/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const enviroment={
+    production: false,
+    apiUrl: "springboot-production-7df8.up.railway.app",
+};
